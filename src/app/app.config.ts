e362f4e6fc5993d3e917dafeb7dbe4bc1ec13166
edrialan.angulo@bbva.com <edrialan.angulo@bbva.com>
2024-11-07 16:19:52 -0600

    se agrega formulario para peticion save al ws

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes,withComponentInputBinding()), 
+    importProvidersFrom(
+      HttpClientModule
+    ),
     provideFirebaseApp(() => 
       initializeApp({
         "projectId": "ng-task-18-ac282", 
